369ccf02bf88274061c1ae9c624cdf7125596aa0
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-06-14 15:37:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function calculateGrade(percentage) {
   //your code here
+	percentage=Math.round(percentage)
+	if(percentage>=90)
+	{
+		return "A"
+	}
+	else if(percentage>=80)
+	{
+		return "B"
+	}
+	else if(percentage>=70)
+	{
+		return "C"
+	}
+	else if(percentage>=60)
+	{
+		return "D"
+	}
+	else
+	{
+		return "F"
+	}
 }
 const percentage = parseInt(prompt("Enter Percentage."));
 alert(calculateGrade(percentage));
